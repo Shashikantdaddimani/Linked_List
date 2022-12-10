2d7458559281c8907e2d8fe74ed55539c345e588
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-10 11:23:27 +0530

    Added:Logic for creating the simple LinkedList

--- src/com/java/linkedlist/LinkedList.java
+++ src/com/java/linkedlist/LinkedList.java
@@ -1,9 +1,39 @@
 package com.java.linkedlist;
-
+/**
+ *  Class to create LinkedList with different methods
+ */
 public class LinkedList {
-
+	Node head;
+/*
+ * Without LinkedList node can't exists so thats why  inside LinkedList  Class we created Node class
+ */
+	class Node {
+		Object data;
+		Node ref;
+		/*
+		 * Declare Parameterized constructor
+		 */
+		Node(Object data) {
+			this.data = data;
+		}
+	}
+	/*Uc-1
+	 * Method Name:Elements
+	 * Logic for creating the simple LinkedList
+	 */
+	public void elements(Object data) {
+		Node newNode = new Node(data);
+		System.out.print(newNode.data+" ");
+	}
+	/*
+	 * Program execution starts from main method
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Linked List program");
+		LinkedList list =new LinkedList();
+		list.elements(56);
+		list.elements(30);
+		list.elements(70);
 	}
 
 }
